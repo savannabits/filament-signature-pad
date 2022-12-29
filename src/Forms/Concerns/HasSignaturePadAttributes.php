4d93d8dd47835d1df16200346fa11c56c1b040a4
coolsam726 <coolsam726@users.noreply.github.com>
2022-12-29 17:03:34 +0000

    Fix styling

--- src/Forms/Concerns/HasSignaturePadAttributes.php
+++ src/Forms/Concerns/HasSignaturePadAttributes.php
@@ -7,69 +7,80 @@ use Closure;
 trait HasSignaturePadAttributes
 {
     protected float|Closure|null $strokeMaxWidth = 2.5;
+
     protected float|Closure|null $strokeMinWidth = 1.0;
+
     protected float|Closure|null $strokeDotSize = 2.0;
+
     protected float|Closure|null $strokeMinDistance = 2.0;
+
     protected string|Closure|null $penColor = 'rgb(0,0,0)';
+
     protected string|Closure|null $backgroundColor = 'rgba(0,0,0,0)';
 
     /**
-     * @param Closure|float $strokeMaxWidth
+     * @param  Closure|float  $strokeMaxWidth
      * @return HasSignaturePadAttributes
      */
     public function strokeMaxWidth(float|Closure $strokeMaxWidth): static
     {
         $this->strokeMaxWidth = $strokeMaxWidth;
+
         return $this;
     }
 
     /**
-     * @param Closure|float $strokeMinWidth
+     * @param  Closure|float  $strokeMinWidth
      * @return HasSignaturePadAttributes
      */
     public function strokeMinWidth(float|Closure $strokeMinWidth): static
     {
         $this->strokeMinWidth = $strokeMinWidth;
+
         return $this;
     }
 
     /**
-     * @param Closure|float $strokeDotSize
+     * @param  Closure|float  $strokeDotSize
      * @return HasSignaturePadAttributes
      */
     public function strokeDotSize(float|Closure $strokeDotSize): static
     {
         $this->strokeDotSize = $strokeDotSize;
+
         return $this;
     }
 
     /**
-     * @param Closure|float $strokeMinDistance
+     * @param  Closure|float  $strokeMinDistance
      * @return HasSignaturePadAttributes
      */
     public function strokeMinDistance(float|Closure $strokeMinDistance): static
     {
         $this->strokeMinDistance = $strokeMinDistance;
+
         return $this;
     }
 
     /**
-     * @param Closure|string $penColor
+     * @param  Closure|string  $penColor
      * @return HasSignaturePadAttributes
      */
     public function penColor(string|Closure $penColor): static
     {
         $this->penColor = $penColor;
+
         return $this;
     }
 
     /**
-     * @param Closure|string $backgroundColor
+     * @param  Closure|string  $backgroundColor
      * @return HasSignaturePadAttributes
      */
     public function backgroundColor(string|Closure $backgroundColor): static
     {
         $this->backgroundColor = $backgroundColor;
+
         return $this;
     }
 
