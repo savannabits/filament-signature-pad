ceffcc0504de87202af89ec1e3658822cd052630
Sam Maosa <smaosa@strathmore.edu>
2023-08-11 17:36:10 +0300

    Documentation Improvement - Re-factored the README docs

--- src/Forms/Concerns/HasSignaturePadAttributes.php
+++ src/Forms/Concerns/HasSignaturePadAttributes.php
@@ -16,7 +16,7 @@ trait HasSignaturePadAttributes
 
     protected string | Closure | null $penColor = 'rgb(0,0,0)';
 
-    protected string | Closure | null $backgroundColor = 'rgba(0,0,0,0)';
+    protected string | Closure | null $backgroundColor = 'rgba(255,255,255, 1)';
 
     protected bool $hideDownloadButtons = false;
 
